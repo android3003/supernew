3f582f510dcde50132642eab988a0aa2a1f2eef4
android3003 <jonoonsorat@gmail.com>
2016-05-20 10:53:05 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -270,7 +270,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {162337997},
+    sudo_users = {97653520},
     admin_users = {},
     disabled_channels = {}
   }
